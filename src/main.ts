fc80d98d2e0c2347654bdd39ce0ec75683d8a3ca
Lucas Barros <lucas.barros@shawandpartners.com>
2019-08-03 09:47:31 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core'
+import { INestApplication } from '@nestjs/common'
+
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
+const swaggerSetup = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+    .setTitle('TBD project')
+    .setDescription('To be announced')
+    .setVersion('1.0')
+    .addTag('tbd')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  swaggerSetup(app)
+  await app.listen(3000)
+}
+bootstrap()
